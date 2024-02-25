9a81fe63fac3b16db4bf4b0358d56edcd20e1d5c
volyas <115228295+volyas@users.noreply.github.com>
2024-02-25 12:58:43 +0800

    Добавила кнопки.

--- Graphify.Client/MainWindow.xaml.cs
+++ Graphify.Client/MainWindow.xaml.cs
@@ -14,9 +14,19 @@ public partial class MainWindow
         ViewModel = viewModel;
         DataContext = viewModel;
         InitializeComponent();
-
         this.WhenActivated(disposables =>
         {
+            this.BindCommand(ViewModel, vm => vm.MoveModeCommand, view => view.MoveModeButton)
+                .DisposeWith(disposables);
+
+            this.BindCommand(ViewModel, vm => vm.CreatePointModeCommand, view => view.CreatePointModeButton)
+                .DisposeWith(disposables);
+
+            this.BindCommand(ViewModel, vm => vm.CreateLineModeCommand, view => view.CreateLineModeButton)
+                .DisposeWith(disposables);
+
+            this.Bind(ViewModel, vm => vm.ReactiveProperty, view => view.ValueBox.Text)
+                .DisposeWith(disposables);
         });
     }
 
