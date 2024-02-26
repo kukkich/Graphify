426400d677c1e6c1e3583cb4d30055a7d0a3267e
Irina Suderevskaya <pm93.suderevskaya@gmail.com>
2024-02-26 21:51:50 +0700

    Добавлен список объектов, без привязки к общим объектам. Демонстрация внешнего вида

--- Graphify.Client/MainWindow.xaml.cs
+++ Graphify.Client/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ using SharpGL;
 namespace Graphify.Client;
 
 public partial class MainWindow     
-{
+{    
     private OpenGL _gl;
     public MainWindow(AppViewModel viewModel)
     {
@@ -18,6 +18,36 @@ public partial class MainWindow
         this.WhenActivated(disposables =>
         {
         });
-    }
 
+        List<Figure> listObjects = new List<Figure>()
+        {
+            new Figure()
+            {
+                Data = "A = (-1.2, 1.33)",
+                Type = "LightBlue"
+            },
+            new Figure()
+            {
+                Data = "B = (3.45, 2.1)",
+                Type = "LightBlue"
+            },
+            new Figure()
+            {
+                Data = "C = (0, 0)",
+                Type = "LightBlue"
+            },
+            new Figure()
+            {
+                Data = "f: Прямая(B, C)",
+                Type = "LightGray"
+            }
+        };
+        this.listGeometryObjects.ItemsSource = listObjects;
+
+    }
+    public class Figure
+    {
+        public string Data { get; set; }
+        public string Type { get; set; }
+    }
 }
