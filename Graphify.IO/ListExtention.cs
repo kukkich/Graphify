19cfe0436171020e215fc7bde8ccc21806f3913f
Salnikov Dmitry <dima.salnikov.2014@inbox.ru>
2024-03-10 00:02:11 +0700

    Еще поменял

--- Graphify.IO/ListExtention.cs
+++ Graphify.IO/ListExtention.cs
@@ -1,10 +1,10 @@
-﻿using System.Numerics;
+using System.Numerics;
 using Graphify.Geometry.GeometricObjects.Points;
 
 namespace Graphify.IO;
 
 public static class ListExtention
 {
-    public static List<Vector2> ToListVector2(this List<Point> points)
+    public static List<Vector2> ToVector2(this List<Point> points)
         => points.Select(point => PointExtention.ToVector2(point)).ToList();
 }
