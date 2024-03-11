fed05f65debafba1dc9d09b170848ca50f32df6a
Salnikov Dmitry <dima.salnikov.2014@inbox.ru>
2024-03-12 01:33:26 +0700

    Поправил расширения

--- Graphify.IO/ListExtention.cs
+++ Graphify.IO/ListExtention.cs
@@ -5,6 +5,9 @@ namespace Graphify.IO;
 
 public static class ListExtention
 {
-    public static List<Vector2> ToVector2(this List<Point> points)
-        => points.Select(point => PointExtention.ToVector2(point)).ToList();
+    public static List<Vector2> ToListVector2(this List<Point> points)
+        => points.Select(PointExtention.ToVector2).ToList();
+
+    public static double[] ToArrayCoordinates(this List<Point> points)
+        => points.SelectMany(PointExtention.ToArray).ToArray();
 }
--- Graphify.IO/PointExtention.cs
+++ Graphify.IO/PointExtention.cs
@@ -8,6 +8,6 @@ public static class PointExtention
     public static Vector2 ToVector2(this Point point)
             => new(point.X, point.Y);
 
-    public static double[] PointToArray(this Point point) 
+    public static double[] ToArray(this Point point) 
             => [point.X, point.Y];
 }
