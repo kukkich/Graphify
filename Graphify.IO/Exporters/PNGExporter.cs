a9ee03510f26831715c93dda424bb8fc59df8682
natasha kun <nkun@smart-consulting.ru>
2024-03-13 23:17:46 +0700

    Переписала PNGExporter, добавила библиотеку Aspose.Imaging

--- Graphify.IO/Exporters/PNGExporter.cs
+++ Graphify.IO/Exporters/PNGExporter.cs
@@ -1,25 +1,24 @@
-using Graphify.Geometry.Export;
-using Graphify.Geometry.GeometricObjects.Interfaces;
-using Graphify.Geometry.GeometricObjects.Points;
-using Graphify.IO.Interfaces;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Drawing; 
-using System.Drawing.Imaging;
+using System.IO;
 using System.Numerics;
+using Aspose.Imaging.ImageOptions;
+using Aspose.Svg;
+using Aspose.Svg.Builder;
+using DynamicData;
+using Graphify.Geometry.Export;
 using Graphify.Geometry.GeometricObjects.Curves;
+using Graphify.Geometry.GeometricObjects.Interfaces;
+using Graphify.Geometry.GeometricObjects.Points;
+using Graphify.Geometry.GeometricObjects.Polygons;
+using Graphify.IO.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Graphify.IO.Exporters
 {
     public sealed class PNGExporter : IExporter
     {
         private readonly ILogger<PNGExporter> _logger;
-
-        private Bitmap _bitmap; 
-
-        private float _pngWidth = 0f; 
-        private float _pngHeight = 0f; 
 
         public PNGExporter(ILogger<PNGExporter> logger)
         {
@@ -28,140 +27,156 @@ namespace Graphify.IO.Exporters
 
         public void Export(IGeometryContext context, string path)
         {
-            CalculateDimensions(context.Points);  
+            var svgDocument = CreateSvgDocument(context);
 
-            _bitmap = new Bitmap((int)_pngWidth, (int)_pngHeight); 
+            var tempSvgPath = Path.GetTempFileName();
+            svgDocument.Save(tempSvgPath);
 
-            using (Graphics g = Graphics.FromImage(_bitmap))
+            using (var image = Aspose.Imaging.Image.Load(tempSvgPath))
             {
-                g.Clear(Color.White); 
-
-                IEnumerable<Point> points = context.Points;
-                IEnumerable<IFigure> figures = context.Figures;
-
-                foreach (Point point in points)
-                {
-                    PointExportData pointExportData = point.GetExportData();
-                    DrawPoint(g, pointExportData);
-                }
-
-                foreach (IFigure figure in figures)
-                {
-                    FigureExportData figureExportData = figure.GetExportData();
-                    DrawFigure(g, figureExportData, figure.ControlPoints);
-                }
+                var pngOptions = new PngOptions();
+                image.Save(path, pngOptions);
             }
-
-            SaveBitmap(path);
         }
 
-        private void CalculateDimensions(IEnumerable<Point> points)
+        private SVGDocument CreateSvgDocument(IGeometryContext context)
         {
+            var svgElements = new SVGSVGElementBuilder();
+            IEnumerable<Point> points = context.Points;
+            IEnumerable<IFigure> figures = context.Figures;
+
             foreach (Point point in points)
             {
-                if (point.Position.X > _pngWidth)
-                    _pngWidth = point.Position.X;
-                if (point.Position.Y > _pngHeight)
-                    _pngHeight = point.Position.Y;
+                PointExportData pointExportData = point.GetExportData();
+                AddPoint(svgElements, pointExportData);
             }
-        }
 
-
-        private void DrawPoint(Graphics graphics, PointExportData dataPoint)
-        {
-            float size = dataPoint.Style.Size;
-            Color color = dataPoint.Style.PrimaryColor;
-            PointCoordinates position = new PointCoordinates(dataPoint.Position.X, dataPoint.Position.Y);
-
-            using (Brush brush = new SolidBrush(color))
+            foreach (IFigure figure in figures)
             {
-                graphics.FillEllipse(brush, position.X - size / 2, position.Y - size / 2, size, size);
+                FigureExportData figureExportData = figure.GetExportData();
+                AddFigure(svgElements, figureExportData, figure.ControlPoints);
             }
+
+            var svgDocument = new SVGDocument();
+            svgElements.Build(svgDocument.FirstChild as SVGSVGElement);
+
+            return svgDocument;
         }
 
-        private void DrawFigure(Graphics g, FigureExportData data, IEnumerable<Point> controlPoints)
+        private void AddPoint(SVGSVGElementBuilder svgElements, PointExportData dataPoint)
+        {
+            svgElements.AddCircle(
+                circle => circle
+                    .Cx(dataPoint.Position.X)
+                    .Cy(dataPoint.Position.Y)
+                    .R(dataPoint.Style.Size)
+                    .Fill(dataPoint.Style.PrimaryColor)
+                    .Transform(t => t.Scale(1, -1))
+                    );
+        }
+
+        private void AddFigure(SVGSVGElementBuilder svgElements, FigureExportData data, IEnumerable<Point> controlPoints)
         {
             switch (data.FigureType)
             {
-                case ObjectType.Line:
-                    DrawLine(g, data, controlPoints);
-                    break;
-
-                case ObjectType.Circle:
-                    
-                    break;
-
-                case ObjectType.Polygon:
-                    break;
-
-                case ObjectType.CubicBezier:
-                    break;
+                case ObjectType.Line: AddLine(svgElements, data, controlPoints.ToList<Point>()); break;
+                case ObjectType.Circle: AddCircle(svgElements, data, controlPoints.ToList<Point>()); break;
+                case ObjectType.Polygon: AddPolygon(svgElements, data, controlPoints.ToList<Point>()); break;
+                case ObjectType.CubicBezier: AddCubicBezier(svgElements, data, controlPoints.ToList<Point>()); break;
             }
         }
 
-
-        private void DrawLine(Graphics graphics, FigureExportData dataLine, IEnumerable<Point> points)
+        private void AddLine(SVGSVGElementBuilder svgElements, FigureExportData dataLine, List<Point> points)
         {
-            if (points == null || points.Count() != 2)
+            if (points.Count != 2)
             {
-                _logger.LogError("Invalid number of points to draw a line.");
-                return;
+                _logger.LogError(
+                    "The number of points to build a line is not equal to 2. The number of points contained: {pointsCount}", points.Count);
+
+                throw new ArgumentException("");
             }
 
-            PointCoordinates point1 = new PointCoordinates(points.ElementAt(0).X, points.ElementAt(0).Y);
-            PointCoordinates point2 = new PointCoordinates(points.ElementAt(1).X, points.ElementAt(1).Y);
-
-            using (Pen pen = new Pen(dataLine.Style.PrimaryColor, (dataLine.Style as CurveStyle ?? CurveStyle.Default).Size))
-            {
-                graphics.DrawLine(pen, point1, point2);
-            }
+            svgElements.AddLine(
+                line => line
+                    .X1(points[0].X)
+                    .Y1(points[0].Y)
+                    .X2(points[1].X)
+                    .Y2(points[1].Y)
+                    .Stroke(dataLine.Style.PrimaryColor)
+                    .StrokeWidth((dataLine.Style as CurveStyle ?? CurveStyle.Default).Size)
+                    .Transform(t => t.Scale(1, -1))
+                    );
         }
 
-        private void DrawCircle(Graphics graphics, FigureExportData dataCircle, IEnumerable<Point> points)
+        private void AddCircle(SVGSVGElementBuilder svgElements, FigureExportData dataCircle, List<Point> points)
         {
-            if (points == null || points.Count() != 2)
+            if (points.Count != 2)
             {
-                _logger.LogError("Invalid number of points to draw a circle.");
-                return;
+                _logger.LogError(
+                    "The number of points to build a circle is not equal to 2. The number of points contained: {pointsCount}", points.Count);
+
+                throw new ArgumentException("");
             }
 
-            float radius = Vector2.Distance(new Vector2(points.ElementAt(0).X, points.ElementAt(0).Y), new Vector2(points.ElementAt(1).X, points.ElementAt(1).Y));
+            var circlePoints = points.ToVector2();
 
-            PointCoordinates center = new PointCoordinates(points.ElementAt(0).X - radius, points.ElementAt(0).Y - radius);
-            RectangleF rect = new RectangleF(center, new SizeF(radius * 2, radius * 2));
+            float radius = Vector2.Distance(circlePoints[0], circlePoints[1]);
 
-            using (Pen pen = new Pen(dataCircle.Style.PrimaryColor, (dataCircle.Style as CurveStyle ?? CurveStyle.Default).Size))
-            {
-                graphics.DrawEllipse(pen, rect);
-            }
+            svgElements.AddCircle(
+                circle => circle
+                    .Cx(circlePoints[0].X)
+                    .Cy(circlePoints[0].Y)
+                    .R(radius)
+                    .Fill(Paint.None)
+                    .Stroke(dataCircle.Style.PrimaryColor)
+                    .StrokeWidth((dataCircle.Style as CurveStyle ?? CurveStyle.Default).Size)
+                    .Transform(t => t.Scale(1, -1))
+                );
         }
 
-        private void DrawCubicBezier(Graphics graphics, FigureExportData dataBezier, IEnumerable<Point> points)
+        private void AddPolygon(SVGSVGElementBuilder svgElements, FigureExportData dataPolygon, List<Point> points)
         {
-            if (points == null || points.Count() != 4)
+            if (points.Count < 3)
             {
-                _logger.LogError("Invalid number of points to draw a cubic bezier.");
-                return;
+                _logger.LogError(
+                    "The number of points to build a Polygon less 3. The number of points contained: {pointsCount}", points.Count);
+
+                throw new ArgumentException("");
             }
 
-            PointCoordinates[] bezierPoints = new PointCoordinates[]
-            {
-                new PointCoordinates(points.ElementAt(0).X, points.ElementAt(0).Y),
-                new PointCoordinates(points.ElementAt(1).X, points.ElementAt(1).Y),
-                new PointCoordinates(points.ElementAt(2).X, points.ElementAt(2).Y),
-                new PointCoordinates(points.ElementAt(3).X, points.ElementAt(3).Y)
-            };
+            double[] pointsSVG = points.SelectMany(x => x.PointToArray()).ToArray();
 
-            using (Pen pen = new Pen(dataBezier.Style.PrimaryColor, (dataBezier.Style as CurveStyle ?? CurveStyle.Default).Size))
-            {
-                graphics.DrawBezier(pen, bezierPoints[0], bezierPoints[1], bezierPoints[2], bezierPoints[3]);
-            }
+            svgElements.AddPolygon(
+                polygon => polygon
+                    .Points(pointsSVG)
+                    .Fill(dataPolygon.Style.PrimaryColor)
+                    .Stroke(Paint.None)
+                    .Transform(t => t.Scale(1, -1))
+                );
         }
 
-
-        private void SaveBitmap(string path)
+        private void AddCubicBezier(SVGSVGElementBuilder svgElements, FigureExportData dataBezier, List<Point> points)
         {
-            _bitmap.Save(path, ImageFormat.Png);
+            if (points.Count != 4)
+            {
+                _logger.LogError(
+                    "The number of points to build a cubic bezier is not equal to 4. The number of points contained: {pointsCount}", points.Count);
+
+                throw new ArgumentException("");
+            }
+
+            svgElements.AddPath(
+                cubicBezier => cubicBezier
+                    .D(
+                    d => d
+                        .M(points[0].X, points[0].Y)
+                        .C(points[1].X, points[1].Y, points[2].X, points[2].Y, points[3].X, points[3].Y)
+                        .Z())
+                    .Fill(Paint.None)
+                    .Stroke(dataBezier.Style.PrimaryColor)
+                    .StrokeWidth((dataBezier.Style as CurveStyle ?? CurveStyle.Default).Size)
+                    .Transform(t => t.Scale(1, -1))
+                );
         }
     }
 }
