e9a1929e1c888865a57a5987ec4c7ce340190e66
Sharero <yaroslav.sumin@mail.ru>
2024-03-14 20:55:37 +0700

    updated Exporter

--- Graphify.IO/Exporters/GraphifyExporter.cs
+++ Graphify.IO/Exporters/GraphifyExporter.cs
@@ -1,46 +1,74 @@
 using Graphify.Geometry.Export;
-using System.Linq;
 using Graphify.Geometry.GeometricObjects.Points;
 using Graphify.Geometry.GeometricObjects.Interfaces;
 using Graphify.IO.Interfaces;
 using Microsoft.Extensions.Logging;
 using System.Text;
 using Newtonsoft.Json;
+using System.Reactive;
 
 namespace Graphify.IO.Exporters;
 
 public class GraphifyExporter(ILogger<GraphifyExporter> logger) : IExporter
 {
     private readonly ILogger<GraphifyExporter> _logger = logger;
+
+    private readonly List<JsonPointObject> _points = [];
+    private readonly List<JsonFigureObject> _figures = [];
+
     private StringBuilder _jsonElements = null!;
 
     public void Export(IGeometryContext context, string path)
     {
+        uint i = 1;
+
         _jsonElements = new StringBuilder();
-        
+
         IEnumerable<Point> points = context.Points;
         IEnumerable<IFigure> figures = context.Figures;
 
-        foreach (IFigure figure in figures)
+        List<Point>? independentPoints = points.Where(p => !p.IsAttached).ToList();
+
+        if (independentPoints is not null)
         {
-            FigureExportData figureExportData = figure.GetExportData();
-            
+            AddPoints(independentPoints, ref i);
         }
 
-        foreach (Point point in points)
+        foreach (IFigure figure in figures)
         {
-            PointExportData pointExportData = point.GetExportData();
-            ExportPoint(pointExportData);
+            FigureExportData exportFigureData = figure.GetExportData();
+
+            uint[] idControlPoints = AddPoints(figure.ControlPoints.ToList(), ref i);
+            uint[] idAttachedPoints = AddPoints(figure.Attached.ToList(), ref i);
+
+            _figures.Add(new JsonFigureObject(
+                                             exportFigureData.FigureType,
+                                             idAttachedPoints,
+                                             idControlPoints,
+                                             exportFigureData.Style));
         }
 
         CreateFile(path);
+
+        _points.Clear();
     }
 
-    private void ExportPoint(PointExportData data) => AddPoint(data);
-    private void AddPoint(PointExportData dataPoint)
+    private uint[] AddPoints(List<Point> points, ref uint i)
     {
-        _jsonElements.Append(JsonConvert.SerializeObject(dataPoint));
+        List<uint> result = [];
+
+        foreach (Point point in points)
+        {
+            PointExportData data = point.GetExportData();
+
+            _points.Add(new JsonPointObject(i, data.Position, data.Style));
+
+            ++i;
+        }
+
+        return [.. result];
     }
+
     private void CreateFile(string path)
     {
         File.WriteAllText(path, _jsonElements.ToString());
