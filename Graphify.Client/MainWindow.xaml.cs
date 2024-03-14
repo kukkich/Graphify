da511fc6d93ca45cc02a8352cb5da216fae89652
zergar5 <artem_kozhanov@mail.ru>
2024-03-15 02:36:15 +0700

    more fixes

--- Graphify.Client/MainWindow.xaml.cs
+++ Graphify.Client/MainWindow.xaml.cs
@@ -59,7 +59,7 @@ public partial class MainWindow
             return;
         }
 
-        ViewModel?.SetEditMode.Execute(EditMode.Rotate);
+        ViewModel?.SetEditMode.Execute(EditMode.Move);
     }
 
     private void CreatePointModeButton_Click(object sender, RoutedEventArgs e)
