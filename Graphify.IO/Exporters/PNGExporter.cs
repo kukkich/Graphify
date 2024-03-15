63617e5e63a40b0972d9ac1b27d7255535f2f6b0
natasha kun <nkun@smart-consulting.ru>
2024-03-15 12:42:08 +0700

    исправление + добавлено сохранение временного файла

--- Graphify.IO/Exporters/PNGExporter.cs
+++ Graphify.IO/Exporters/PNGExporter.cs
@@ -4,32 +4,33 @@ using Aspose.Svg.Saving;
 using Graphify.Geometry.GeometricObjects.Interfaces;
 using Graphify.IO.Interfaces;
 
-namespace Graphify.IO.Exporters
+namespace Graphify.IO.Exporters;
+
+public sealed class PNGExporter : IExporter
 {
-    public sealed class PNGExporter : IExporter
+    private readonly SVGExporter _svgExporter;
+
+    public PNGExporter(SVGExporter svgExporter)
     {
-        private readonly SVGExporter _svgExporter; 
+        _svgExporter = svgExporter;
+    }
 
-        public PNGExporter(SVGExporter svgExporter)
-        {
-            _svgExporter = svgExporter; 
-        }
+    public void Export(IGeometryContext context, string path)
+    {
+        string svgPath = Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path) + "_temporary.svg");
 
-        public void Export(IGeometryContext context, string path)
-        {
-            _svgExporter.Export(context, path);
+        _svgExporter.Export(context, svgPath);
 
-            ConvertSvgToPng(path);
-        }
+        ConvertSvgToPng(path, svgPath);
 
-        private static void ConvertSvgToPng(string svgPath)
-        {
-            using (var document = new SVGDocument(svgPath))
-            {
-                var pngSaveOptions = new ImageSaveOptions();
+        File.Delete(svgPath);
+    }
 
-                Converter.ConvertSVG(document, pngSaveOptions, Path.ChangeExtension(svgPath, ".png"));
-            }
-        }
+    private static void ConvertSvgToPng(string path, string svgPath)
+    {
+        using var document = new SVGDocument(svgPath);
+        var pngSaveOptions = new ImageSaveOptions();
+
+        Converter.ConvertSVG(document, pngSaveOptions, path);
     }
 }
